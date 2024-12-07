db5efd700ccc02c3558eed9f7512ea7b37db7f0e
Infinite Monkey <icarogabryel2001@gmail.com>
2024-12-07 11:48:22 -0300

    person model created

--- src/backend/tasks/models.py
+++ src/backend/tasks/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
 
-class Task(models.Model):
-    title = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+    cel_number = models.CharField(max_length=20)
+    email = models.EmailField(max_length=200)
+    birth_date = models.DateField()
 
     def __str__(self):
-        return self.title
+        return self.name
 
     class Meta:
-        db_table = 'tasks'
+        verbose_name = 'Person'
+        verbose_name_plural = 'Persons'
+        db_table = 'person'
